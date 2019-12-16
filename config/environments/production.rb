cd6bf9ec6f2a2e77e2e6dee543bd9acadb37fb00
rcmll <alice_rc@live.fr>
2019-12-16 15:01:53 +0100

    change domain in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'alicercmll.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'alicercmll.dev' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'alicercmll.herokuapp.com',
+    domain:               'alicercmll.dev',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
