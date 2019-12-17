ff5408ca2552af639035d877653f6e8e1a94edfd
rcmll <alice_rc@live.fr>
2019-12-17 16:55:23 +0100

    front profil

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -263,3 +263,14 @@ $('.contact-form').find('.form-control').blur(function() {
 (function($){
         new WOW().init();
     })(jQuery);
+
+// text anim
+
+import Typed from 'typed.js';
+
+var options = {
+  strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+  typeSpeed: 40
+};
+
+var typed = new Typed('.element', options);
